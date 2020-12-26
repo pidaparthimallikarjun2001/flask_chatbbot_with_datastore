c13fdfc81b18f4773337ed4f5a54c937ee36bf80
pidaparthimallikarjun2001 <65111739+pidaparthimallikarjun2001@users.noreply.github.com>
2020-12-26 12:02:34 +0530

    Update app.py

--- flask chatbot/app.py
+++ flask chatbot/app.py
@@ -3,53 +3,59 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
-app = Flask(__name__)        
+app = Flask(__name__)        #creating object 
+#references this file : __name__
+
+#Setting up DB
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chats.db'
 
 db = SQLAlchemy(app)
 
+#MVC: Model(structure) - View - Controller
+
+#We have to define classes, each class variable is considered as a piece of data in the database
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.Text, nullable = False)
     date_sent = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
 
-    def __repr__(self):
+    def __repr__(self): #It prints out whenever a new chat is created
         return 'Chat ' + str(self.id)
 
 
 #Dummy Data
 
-all_chats = [
-    {
-        'content': 'Hai there!'
-    },
-    {
-        'content': 'Hello, who are you?'
-    },
-    {
-        'content': 'I am a chatbot'
-    },
-    {
-        'content': 'What can you do?'
-    },
-    {
-        'content': 'For now, I can tell you a quote'
-    },
-    {
-        'content': 'Thank you so much'
-    },
-    {
-        'content': 'You are welcome'
-    },
-    {
-        'content': 'bye'
-    },
-    {
-        'content': 'bye'
-    }
-]
+# all_chats = [
+#     {
+#         'content': 'Hai there!'
+#     },
+#     {
+#         'content': 'Hello, who are you?'
+#     },
+#     {
+#         'content': 'I am a chatbot'
+#     },
+#     {
+#         'content': 'What can you do?'
+#     },
+#     {
+#         'content': 'For now, I can tell you a quote'
+#     },
+#     {
+#         'content': 'Thank you so much'
+#     },
+#     {
+#         'content': 'You are welcome'
+#     },
+#     {
+#         'content': 'bye'
+#     },
+#     {
+#         'content': 'bye'
+#     }
+# ]
 
 
 
@@ -61,11 +67,12 @@ def index():
 
 @app.route('/chats', methods = ['GET', 'POST'])
 def chats():
+    # Just before like rendering the template, we are retrieving the data entered by the user in the form and we are storing it in the database
     if request.method == 'POST':
         chat_content = request.form['content']
         new_chat = Chat(content = chat_content)
-        db.session.add(new_chat)
-        db.session.commit()
+        db.session.add(new_chat)    #only for current session
+        db.session.commit()     #permanent
         # replying to the user accordingly
         if chat_content.lower() == "hello" or chat_content.lower() == "hai":
             botreplytoinput = Chat(content = "Hai, there!")
@@ -107,20 +114,20 @@ def chats():
 
         
 
-        return redirect('/chats',)
+        return redirect('/chats')
     else:
         all_chats = Chat.query.order_by(Chat.date_sent).all()
-        return render_template('chats.html', chats = all_chats)     
+        return render_template('chats.html', chats = all_chats)     #we are like sending some data through a variable called chats in the form of list of dictionaries. We can access this data in chats.html
 
 
-@app.route('/chats/delete/<int:id>')
+@app.route('/chats/delete/<int:id>')    #when we get to this url, then we get the chat id and delete it from db and redirect to /chats route 
 def delete(id):
     chat = Chat.query.get_or_404(id)
     db.session.delete(chat)
     db.session.commit()
     return redirect('/chats')
 
-@app.route('/chats/edit/<int:id>', methods = ['GET', 'POST'])
+@app.route('/chats/edit/<int:id>', methods = ['GET', 'POST'])       #when we get to this url, then we get the chat id and update it from db and redirect to /chats route 
 def edit(id):
     chat = Chat.query.get_or_404(id)
     if request.method == 'POST':
@@ -130,7 +137,8 @@ def edit(id):
     else:
         return render_template('edit.html', chat = chat)
 
-
+#GET to get data  from a specified resource
+#POST to send data to a server to create/update a resource
 
 
 if __name__ == "__main__":
